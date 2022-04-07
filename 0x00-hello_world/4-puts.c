0f89859a4242159f04e6d6228aaa8e7f8aac3273
J.E. Omole <joshdavidson.oj@gmail.com>
2022-04-07 12:09:11 -0700

    Print a string followed by a newline with the puts() function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+  * main - This is the entry point of the program
+  *
+  * Return - ALways 0 (SUCCESS)
+  */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
