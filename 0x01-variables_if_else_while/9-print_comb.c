7aa648628f1733cb90775704000e38366c643576
J.E. Omole <joshdavidson.oj@gmail.com>
2022-04-08 14:36:35 -0700

    Print all possible combinations of single-digit numbers, in ascending order and delimited with a comma and space

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  *
+  * Print all possible combinations of single-digit numbers,
+  * in ascending order and delimited with a comma and space
+  * Return: 0
+  */
+int main(void);
+{
+	int sing_digit;
+
+	sing_digit = '0';
+	while (sing_digit <= '9')
+	{
+		if (sing_digit != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		putchar(sing_digit);
+		sing_digit++;
+	}
+	return (0);
+}
