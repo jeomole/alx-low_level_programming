8715e5686a75140bfddf55a8c73f0ced42fb0879
J.E. Omole <joshdavidson.oj@gmail.com>
2022-04-08 14:16:03 -0700

    Prints the lowercase letters of the alphabet in reverse, followed by newline

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  *
+  * Prints the lowercase letters of the alphabet in reverse, followed by newline
+  * Return: 0
+  */
+int main(void)
+{
+	char ch;
+	
+	ch = 'z';
+	while (ch >= 'a')
+	{
+		putchar(ch);
+		ch--;
+	}
+	putchar('\n');
+	return (0);
+}
