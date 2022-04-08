68950db32b6cfaf09c145a47ba4ccc0cac56c9a4
J.E. Omole <joshdavidson.oj@gmail.com>
2022-04-08 13:25:08 -0700

    Print the letters of the alphabet in lowercase followed by a newline

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  *
+  * Return: Return 0
+  */
+int main(void)
+{
+	char ch;
+
+	ch = 'a'
+		while(ch <= 'z')
+		{
+			putchar(ch);
+			ch++;
+		}
+	putchar('\n');
+	return (0);
+}
