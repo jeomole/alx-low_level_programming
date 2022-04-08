72893037359bcb2a7bbf4c5640cc526a6b23aa00
J.E. Omole <joshdavidson.oj@gmail.com>
2022-04-08 08:17:19 -0700

    Print to the stdout the last number of the random value assigned to n and whether it is greater than 5, less than 6 or is 0

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+  * main - Entry point of program
+  *
+  * Return: Returns 0
+  */
+int main(void)
+
+{
+
+		int n;
+
+
+
+			srand(time(0));
+
+				n = rand() - RAND_MAX / 2;
+		int m = n % 10;
+					if ((n % 10) > 5)
+					{
+						printf("Last digit of %d is %d and is greater than 5\n", n, m);
+					}
+					else if ((n % 10) == 0)
+					{
+						printf("Last digit of %d is 0 and is 0\n", n, m);
+					}
+					else
+					{
+						printf("Last digit of %d is %d and is less than 6 and not 0\n", n, m);
+					}
+					return (0);
+
+}
