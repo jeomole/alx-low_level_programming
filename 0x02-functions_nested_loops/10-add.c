0f08b8296465cef2446ce1a6a2cdef3e882af755
J.E. Omole <joshdavidson.oj@gmail.com>
2022-04-13 11:52:34 -0700

    Adds two numbers and returns the result

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -4,13 +4,9 @@
  * add - returns the sum of its parameters
  * @a: Integer to be added
  * @b: Integer to be added
- * @c : Sum of the numbers
  * Return: Returns 0
  */
 int add(int a, int b)
 {
-	int c = (a + b);
-
-	_putchar('0' + c);
-	return (c);
+	return (a + b);
 }
