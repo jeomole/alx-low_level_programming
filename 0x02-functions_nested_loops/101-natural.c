0b6a814b7f77b7c479c0f1cb15ade70b4ed78147
J.E. Omole <joshdavidson.oj@gmail.com>
2022-04-13 21:07:12 -0700

    Print sum of all multiples of 3 or 5 below 1024, followed by a new line

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -9,11 +9,12 @@ int main(void)
 {
 	int c = 0;
 	int sum = 0;
+
 	while (c < 1024)
 	{
-		 if (c % 3 == 0 || c % 5 == 0)
+		if (c % 3 == 0 || c % 5 == 0)
 		 {
-			 sum = sum + c;
+			sum = sum + c;
 		 }
 		 c++;
 	}
