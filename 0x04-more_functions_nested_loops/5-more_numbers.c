463650e7d1a1909f1ecbe9bc4d88d173e638688a
J.E. Omole <joshdavidson.oj@gmail.com>
2022-04-14 17:56:20 -0700

    Print 0 to 14 ten times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+  * more_numbers - Prints 0 to 14 ten times
+  * @freq: Number of times to be printed
+  * @num: Number
+  * Return: Void
+  */
+void more_numbers(void)
+{
+	int freq;
+	int num;
+
+	freq = 0;
+	while (freq <= 9)
+	{
+		num = 0
+		while (num <= 14)
+		{
+			_putchar(num);
+			num++;
+		}
+		_putchar(10)
+		freq++;
+	}
+}
