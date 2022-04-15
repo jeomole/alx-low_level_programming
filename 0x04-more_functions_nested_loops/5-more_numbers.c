2dbbe3e00ab33c39a7dddc31aaa4f29f8ca35925
J.E. Omole <joshdavidson.oj@gmail.com>
2022-04-14 18:05:18 -0700

    Print 0 to 14 ten times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,13 +11,21 @@ void more_numbers(void)
 	int freq;
 	int num;
 
-	freq = '0';
-	while (freq <= '9')
+	freq = 0;
+	while (freq <= 9)
 	{
-		num = '0'
-		while (num <= '14')
+		num = 0
+		while (num <= 14)
 		{
+			if (num >= 10)
+			{
+				_putchar((num / 10) + '0');
+				_putchar((num % 10) + '0')
+			}
+			else
+			{
 			_putchar(num);
+			}
 			num++;
 		}
 		_putchar(10)
