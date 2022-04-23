74b73215a88a41d8231de23e2eb2525ad8fddc32
J.E. Omole <joshdavidson.oj@gmail.com>
2022-04-23 04:30:41 -0700

    copy string into another

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -4,6 +4,7 @@
   * _strncpy - copies a string
   * @dest: destination string
   * @src: source string
+  * @n: number of bytes to copy from source string
   * Return: pointer to dest
   */
 char *_strncpy(char *dest, char *src, int n)
