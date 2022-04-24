cbf8a0e247eb65fe80dd8a6150e95d8fbf146b25
J.E. Omole <joshdavidson.oj@gmail.com>
2022-04-24 02:28:27 -0700

    capitalises new words of a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,13 +12,14 @@ char *cap_string(char *str)
 	/* Array of ASCII codes for characters that are valid as word separators */
 	char separator[] = {44, 59, 46, 33, 63, 34, 40, 41, 123, 125, 32, 10, 9};
 
+	i = 0;
 	while (str[i] != '\0')
 	{
 		if (i == 0 && str[i] >= 97 && str[i] <= 122)
 		{
 			str[i] = str[i] - 32;
 		}
-		for (j = 0; separator[j] != '\0'; s++)
+		for (j = 0; separator[j] != '\0'; j++)
 		{
 			if (separator[j] == str[i] && str[i + 1] >= 97 && str[i + 1] <= 122)
 				str[i + 1] = str[i + 1] - 32;
