1d6973c823d954821f7779b4db41b47cd11c226c
J.E. Omole <joshdavidson.oj@gmail.com>
2022-04-24 11:45:07 -0700

    encode string with rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+  * rot13 - encodes a string using rot13
+  * @str: string to be encoded
+  * Return: pointer to the string encoded
+  */
+char *rot13(char *)
+{
+	int i, j;
+	char alp[] = {aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ};
+	char rot[] = {nNoOpPqQrRsStTuUvVwWxXyYzZaAbBcCdDeEfFgGhHiIjJkKlLmM};
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		j = 0;
+		while (alp[j) != '\0')
+		{
+			if (str[i] == alp[j])
+			{
+				str[i] = rot[j];
+			}
+			j++;
+		}
+		i++;
+	}
+	return (str);
+}
+	
