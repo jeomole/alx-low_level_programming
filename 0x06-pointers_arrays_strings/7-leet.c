3fa6c8b907415ea97fc30f583bdd1f3656f85007
J.E. Omole <joshdavidson.oj@gmail.com>
2022-04-24 10:15:32 -0700

    encodes a string in leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,22 +7,19 @@
   */
 char *leet(char *str)
 {
-	int index;
+	int i, j;
+	char alpha[] == "aAeEoOtTlL";
+	char leet[] == "4433007711";
 
 	index = 0;
-	while (str[index] != '\0')
+	while (str[i] != '\0')
 	{
-		if (str[index] == 'a' || str[index] == 'b')
-			str[index] = '4';
-		else if (str[index] == 'e' || str[index] == 'E')
-			str[index] = '3';
-		else if (str[index] == 'o' || str[index] == 'O')
-			str[index] = '0';
-		else if (str[index] == 't' || str[index] == 'T')
-			str[index] = '7';
-		else if (str[index] == 'l' || str[index] == 'L')
-			str[index] = '1';
-		index++;
+		j = 0;
+		while (alpha[j] != '\0')
+		{
+			if (str[i] == alpha[j])
+				str[i] = leet[j];
+		j++;
+		}
+		i++;
 	}
-	return (str);
-}
