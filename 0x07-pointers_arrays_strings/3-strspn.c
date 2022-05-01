84d89a5a3b9ec66d7ff329a69b34dbcf4bdd4489
J.E. Omole <joshdavidson.oj@gmail.com>
2022-05-01 04:24:02 -0700

    header file

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+  * _strspn - gets the length of a prefix substring
+  * @s: input
+  * @accept: input
+  * Return: number of bytes
+  */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int match = 0;
+	int i;
+
+	while (*s)
+	{
+		for (r = 0; accept[i]; i++)
+		{
+			if (*s == accept[i])
+			{
+				match++;
+				break;
+			}
+			else if (accept[i + 1] == '\0')
+				return (n);
+		}
+		s++;
+	}
+	return (n);
+}
