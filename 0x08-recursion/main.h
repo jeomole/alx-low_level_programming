ca4ddee7c9ffbcad50c5ff689e4930f0c0c03e42
J.E. Omole <joshdavidson.oj@gmail.com>
2022-05-03 00:16:38 -0700

    header file

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -7,5 +7,7 @@ void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int _strlen_recursion(char *s);
 int _strlen_recursion(char *s);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
 
 #endif
