a4bbc6c7df55b20f54b08588e782fc4cb939ba21
J.E. Omole <joshdavidson.oj@gmail.com>
2022-05-05 05:09:43 -0700

    multiply two integers passed as arguments to the program

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+  * main - multiples two integers passed to the program
+  * @argc: argument count
+  * @argv: argument vector
+  *
+  * Return: 1, if the program does not receive two arguments
+  * 0, on success
+  */
+int main(int argc, char **argv)
+{
+	if (argc == 3)
+	{
+		printf("%d\n", argv[1] * argv[2]);
+		return (0);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+}
