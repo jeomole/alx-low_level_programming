946d3e32ebb4cae03cfbd3f2afbfa7260eb23ec5
J.E. Omole <joshdavidson.oj@gmail.com>
2022-05-06 19:32:24 -0700

    create a copy of a string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+  * _strdup - returns a pointer to a newly allocated space in memory,
+  * which contains a copy of the string given as a parameter.
+  * @str: the string given as a parameter
+  *
+  * Return: returns a pointer to the duplicated string
+  * returns a pointer to the duplicated string
+  */
+char *_strdup(char *str)
+{
+	char *newstr;
+	int i, size;
+
+	if (str == NULL)
+	return (NULL);
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		size = i + 1;
+		i++;
+	}
+
+	newstr = malloc(size * sizeof(char));
+
+	if (newstr == NULL)
+	return (NULL);
+
+	for (i = 0; i < size; i++)
+	{
+		newstr[i] = str[i]
+	}
+	return (newstr);
+}
