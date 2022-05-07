cc0a0b146eeb7002acc28d25e33672aadacc664d
J.E. Omole <joshdavidson.oj@gmail.com>
2022-05-06 18:40:04 -0700

    create an array of chars and initialize it with a specific char

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -19,13 +19,15 @@ char *create_array(unsigned int size, char c)
 
 	s = malloc(size * sizeof(char));
 
+	if (s == NULL)
+	{
+		return (NULL);
+	}
 	for (i = 0; i < size; i++)
 	{
 		s[i] = c;
 	}
 	s[i] = '\0';
-	
-	free(s);
 
 	return (s);
 }
