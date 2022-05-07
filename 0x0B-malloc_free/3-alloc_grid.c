913e47f66dc4e25cc8bee1509f8e2e6be4240aab
J.E. Omole <joshdavidson.oj@gmail.com>
2022-05-06 21:41:03 -0700

    create a 2d array of integers

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h"
+
+/**
+  * alloc_grid - creates a 2d array of integers
+  * @height: number of rows
+  * @width: number of columns
+  * Return: pointer to the 2d array, NULL on failure
+  * NULL, if width or height is 0 or negative.
+  */
+int **alloc_grid(int width, int height)
+{
+	char **grid;
+	int i, j;
+	
+	if (width == 0 || height == 0)
+		return (NULL);
+
+	grid = malloc(height * width * sizeof(int));
+
+	if (grid == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0, j < width, j++)
+		{
+			grid[i][j] = 0;
+		}
+	}
+	return (grid);
+}
