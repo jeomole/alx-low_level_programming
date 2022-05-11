d2aff0cc2934bfa8e794e63ab6dd389667bcd43d
J.E. Omole <joshdavidson.oj@gmail.com>
2022-05-10 21:45:20 -0700

    allocate memory for an array of ints

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+
+#include "main.h"
+
+#include <stdlib.h>
+
+/**
+ * array_range - creates an array of integers.
+ * @min: first integer
+ * @max: last integer
+ *
+ * Return: A pointer to newly allocated memory, NULL on failure
+ */
+int *array_range(int min, int max)
+{
+	int i, len;
+	int *arr;
+
+	if (min > max)
+		return (NULL);
+
+	len = max - min + 1;
+	a = malloc(sizeof(int) * len);
+	if (a == NULL)
+		return (NULL);
+
+	for (i = 0; i < l; i++, min++)
+	{
+		a[i] = min;
+	}
+	return (a);
+}
