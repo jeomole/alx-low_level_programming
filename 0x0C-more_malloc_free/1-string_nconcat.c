55b2faf7d03de6028e7dadae3782a1c974e246ac
J.E. Omole <joshdavidson.oj@gmail.com>
2022-05-10 20:32:33 -0700

    concatenate two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -29,7 +29,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (n > len2)
 	n = len2;
 
-	newstr = malloc(size of char * (len1 + n + 1));
+	newstr = malloc(sizeof(char) * (len1 + n + 1));
 	if (newstr == NULL)
 		return (NULL);
 
