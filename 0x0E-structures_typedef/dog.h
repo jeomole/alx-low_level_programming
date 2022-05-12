0990467d0d13f69e7c335386d6049d2e6ca94061
J.E. Omole <joshdavidson.oj@gmail.com>
2022-05-12 15:21:34 -0700

    header file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+  * struc dog - Dog data
+  * @name: Name of dog
+  * @age: Age of dog
+  * @owner: Name of dog's owner
+  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
