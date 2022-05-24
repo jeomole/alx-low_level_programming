4268dbdf227578f767b9c0d289254434922f175e
J.E. Omole <joshdavidson.oj@gmail.com>
2022-05-24 06:03:46 -0700

    64-bit program in assembly that prints Hello, Holberton

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,20 @@
+global main
+
+; the text section
+section .text
+
+main:
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, msg
+	mov rdx, msglen
+	syscall
+
+	mov rax, 60
+	mov rdi, 0
+	syscall
+
+; the data section
+section .rodata
+	msg: db "Hello, Holberton", 10
+	msglen: equ $ - msg
